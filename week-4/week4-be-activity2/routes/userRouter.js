d48fd8b1afcaf028a1364f3673623fb5060fef8e
Saed Abukar <saed.a.abukar@gmail.com>
2024-09-13 11:02:27 +0300

    Friday BE Group Activity 2

---
+++ week-4/week4-be-activity2/routes/userRouter.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
+
+// GET /users
+router.get("/", getAllUsers);
+
+// POST /users
+router.post("/", createUser);
+
+// GET /users/:userId
+router.get("/:userId", getUserById);
+
+// PUT /users/:userId
+router.put("/:userId", updateUser);
+
+// DELETE /users/:userId
+router.delete("/:userId", deleteUser);
+
+// Update car using PATCH
+// router.patch('/:carId', patchCar)
+
+module.exports = router;
